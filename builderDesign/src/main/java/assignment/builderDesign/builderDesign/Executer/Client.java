bfe113ff3e7172702ee8f632009effd9a1511cdf
Krishna-web-dev <69578860+Krishna-web-dev@users.noreply.github.com>
2022-02-13 12:59:13 +0530

    Add files via upload

---
+++ builderDesign/src/main/java/assignment/builderDesign/builderDesign/Executer/Client.java
@@ -0,0 +1,20 @@
+package assignment.builderDesign.builderDesign.Executer;
+
+import assignment.builderDesign.builderDesign.Product.Student;
+
+public class Client<student2> {
+
+    public static void main(String[] args) {
+
+        Student student1 = new Student.StudentBuilder("Krishna", "Verma") // mandatory parameters
+                .addAge("90") // optional
+                .addGender("M") // optional
+                .addHasExperience(true) // optional
+                .build(); // to get back student option
+
+        System.out.println(student1);
+
+    }
+
+
+}
---
+++ builderDesign/src/test/java/assignment/builderDesign/builderDesign/BuilderDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package assignment.builderDesign.builderDesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuilderDesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
